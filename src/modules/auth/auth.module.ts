6429a61fc017ebb5b563341bec6b429670fbd882
John Mason <johnmsn22@gmail.com>
2026-04-17 15:44:37 +0100

    removed unused import

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { StaffModule } from '../staff/staff.module';
 import { JwtModule } from '@nestjs/jwt';
 import { config } from '../../utils/config';
 import { StaffService } from '../staff/staff.service';
 
 @Module({
   imports: [
-    StaffModule,
     JwtModule.register({
       global: true,
       secret: config.JWT_SECRET,
